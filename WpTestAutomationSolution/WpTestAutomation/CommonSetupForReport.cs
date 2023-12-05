077feb5f3ee22623b43e06df268e279654507a47
wptestdemo <publishingword55@gmail.com>
2023-12-05 02:05:06 -0800

    Added test automation sln and project

---
+++ WpTestAutomationSolution/WpTestAutomation/CommonSetupForReport.cs
@@ -0,0 +1,29 @@
+﻿using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WpTestAutomation
+{
+ 
+    public class CommonSetupForReport
+    {
+        public static ExtentReports extent;
+
+       
+        public  CommonSetupForReport()
+        {
+            extent = new ExtentReports();
+
+            var htmlreporter = new ExtentSparkReporter(@"C:\\Results\\" + DateTime.Now.ToString("_MMddyyyy_hhmmtt") + ".html");
+            extent.AttachReporter(htmlreporter);
+            extent.AddSystemInfo("Host Name", "Sachi");
+            extent.AddSystemInfo("Environment", "QA-Integration");
+            extent.AddSystemInfo("User Name", "Wp Automation");
+        }
+    }
+}
---
+++ WpTestAutomationSolution/WpTestAutomation/HomePagesTests.cs
@@ -0,0 +1,111 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpTestAutomation
+{
+    [TestClass]
+    public class HomePagesTests //: CommonSetupForReport
+    {
+        public IWebDriver driver;
+        public static ExtentTest test;
+        public TestContext TestContext { get; set; }
+        public static ExtentReports extent;
+
+
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            extent = new ExtentReports();
+
+            var htmlreporter = new ExtentSparkReporter(System.IO.Directory.GetCurrentDirectory()+"\\Test-Results"+".html");
+            extent.AttachReporter(htmlreporter);
+            extent.AddSystemInfo("Host Name", "Sachin");
+            extent.AddSystemInfo("Environment", "QA-Integration");
+            extent.AddSystemInfo("User Name", "Wp Automation");
+        }
+
+        [TestMethod]
+        public void VerifyHomePageHeader()
+        {
+            try
+            {
+                test = null;
+                test = extent.CreateTest(TestContext.TestName).Info(TestContext.TestName);
+                driver = new ChromeDriver();
+                test.Log(Status.Info, "Chrome browser opend");
+                driver.Manage().Window.Maximize();
+                test.Log(Status.Info, "Maximize Chrome browser");
+                driver.Navigate().GoToUrl("http://localhost:92/");
+                test.Log(Status.Info, "Navigate to http://localhost:92/");
+
+                var header1 = driver.FindElement(By.Id("title")).Text;
+
+                Assert.AreEqual("Word Publishing", header1);
+                test.Log(Status.Pass, "Home page Header verification Pass");
+            }
+            catch (Exception ex)
+            {
+                var aaa = System.IO.Directory.GetCurrentDirectory();
+                ((ITakesScreenshot)driver)
+                .GetScreenshot().SaveAsFile(TestContext.TestName + ".png", ScreenshotImageFormat.Png);
+                test.Log(Status.Fail, "Snapshot below: " + test.AddScreenCaptureFromPath(System.IO.Directory.GetCurrentDirectory() + "\\" + TestContext.TestName + ".png"));
+                test.Log(Status.Fail, ex);
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void VerifyUserISAbleToNavigateLearnMorePageSuccessfully()
+        {
+            try
+            {
+                test = null;
+                test = extent.CreateTest(TestContext.TestName).Info(TestContext.TestName);
+                driver = new ChromeDriver();
+                test.Log(Status.Info, "Chrome browser opend");
+                driver.Manage().Window.Maximize();
+                test.Log(Status.Info, "Maximize Chrome browser");
+                driver.Navigate().GoToUrl("http://localhost:92/");
+                test.Log(Status.Info, "Navigate to http://localhost:92/");
+
+                driver.FindElement(By.XPath("(//*[@href='https://www.wpsgp.com/company'])[5]")).Click();
+                Thread.Sleep(2000);
+                var pageTitle = driver.Title;
+                Assert.AreEqual("Company | Word Publishing", pageTitle);
+                test.Log(Status.Pass, "Company | Word Publishing page loaded successfully");
+            }
+            catch (Exception ex)
+            {
+                var aaa = System.IO.Directory.GetCurrentDirectory();
+                ((ITakesScreenshot)driver)
+                .GetScreenshot().SaveAsFile(TestContext.TestName + ".png", ScreenshotImageFormat.Png);
+                test.Log(Status.Fail, "Snapshot below: " + test.AddScreenCaptureFromPath(System.IO.Directory.GetCurrentDirectory() + "\\" + TestContext.TestName + ".png"));
+                test.Log(Status.Fail, ex);
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestCleanup]
+        public void TestClose()
+        {
+
+           driver.Close();
+
+        }
+
+        [ClassCleanup()]
+        public static void ClassCleanup() { 
+
+            extent.Flush();
+
+        }
+    }
+}
